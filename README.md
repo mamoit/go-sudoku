04642b26b4a69583db637b9836f56d1e8c42ea5a
Miguel Almeida <migaxmoitax@gmail.com>
2019-06-07 13:38:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a brute force sudoku solver implemented as an exercise to make
+myself finally start writing something in golang.
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func Printboard(board [9][9]int) {
+	for x := 0; x < 9; x++ {
+		for y := 0; y < 9; y++ {
+			if board[x][y] == 0 {
+				fmt.Print("_")
+			} else {
+				fmt.Print(board[x][y])
+			}
+			if y == 2 || y == 5 {
+				fmt.Print("|")
+			} else if y == 8 {
+				fmt.Print("\n")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		if x == 2 || x == 5 {
+			fmt.Println("-----+-----+-----")
+		}
+	}
+}
+
+func main() {
+	csvFile, _ := os.Open("board.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	var board [9][9]int
+	for x := 0; x < 9; x++ {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		for y := 0; y < 9; y++ {
+			board[x][y], _ = strconv.Atoi(line[y])
+		}
+	}
+
+	Printboard(board)
+	fmt.Println()
+	Solver(board)
+}
+
+func CheckValid(board [9][9]int, x int, y int) []int {
+	var invalid []int
+
+	for a := 0; a < 9; a++ {
+		// Check column
+		if board[a][y] != 0 {
+			invalid = append(invalid, board[a][y])
+		}
+		// Check row
+		if board[x][a] != 0 {
+			invalid = append(invalid, board[x][a])
+		}
+	}
+
+	// Check 3x3 square
+	x_i := x / 3
+	y_i := y / 3
+
+	for i := x_i * 3; i < (x_i+1)*3; i++ {
+		for j := y_i * 3; j < (y_i+1)*3; j++ {
+			if board[i][j] != 0 {
+				invalid = append(invalid, board[i][j])
+			}
+		}
+	}
+
+	sort.Ints(invalid)
+
+	var valid []int
+	i := 0
+
+	for a := 1; a <= 9; a++ {
+		for invalid[i] < a {
+			if len(invalid)-1 == i {
+				break
+			}
+			i++
+		}
+		if invalid[i] != a {
+			valid = append(valid, a)
+		}
+	}
+
+	return valid
+}
+
+func Solver(board [9][9]int) bool {
+	for x := 0; x < 9; x++ {
+		for y := 0; y < 9; y++ {
+			if board[x][y] == 0 {
+				valid := CheckValid(board, x, y)
+				for i := 0; i < len(valid); i++ {
+					board[x][y] = valid[i]
+					if Solver(board) == true {
+						return true
+					}
+				}
+				board[x][y] = 0
+				return false
+			}
+		}
+	}
+	Printboard(board)
+
+	return true
+}
